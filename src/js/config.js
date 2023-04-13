30d03b6f1b260e1a211d9ef4c1aab911cf399595
vanragas <sagar.samundar@gmail.com>
2023-04-13 11:51:28 +0530

    First commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '0c6b2214-c736-495e-bfb5-c11f18f38f5c';
+export const MODAL_CLOSE_SEC = 2.5;
